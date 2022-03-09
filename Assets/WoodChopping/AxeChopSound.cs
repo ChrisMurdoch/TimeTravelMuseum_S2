960ead79e745f4229954c84f29af5ef7754b6c28
Adam Seidner <42008138+abutler2@users.noreply.github.com>
2022-03-09 15:37:29 -0500

    Added Chopping sound effect
    Added the "chop" sound effect, Needs the object being chopped to be tagged as "Tree".

---
+++ Assets/WoodChopping/AxeChopSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeChopSound : MonoBehaviour
+{
+    public AudioSource chopSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        chopSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    { 
+        
+        if (collision.gameObject.tag == "Tree")
+        {
+           chopSource.Play(); 
+        }
+    }
+}
